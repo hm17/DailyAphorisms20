bf05f1a0d71f70b1a824a84d7e23d92d9605e762
HM <hello@hazella.co>
2019-03-05 23:02:46 -0600

    Added initial activities

--- app/src/main/java/com/hm/dailyaphorisms20/GetAphorismsActivity.java
+++ app/src/main/java/com/hm/dailyaphorisms20/GetAphorismsActivity.java
@@ -3,11 +3,11 @@ package com.hm.dailyaphorisms20;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class DisplayAphorismsActivity extends Activity {
+public class GetAphorismsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aphorisms);
+        setContentView(R.layout.activity_get_aphorisms);
     }
 }
